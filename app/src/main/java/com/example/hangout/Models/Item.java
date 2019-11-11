7324208b3725dad7d74692b5df70570e2536a8ea
harinaveen01 <harinaveen01@gmail.com>
2019-11-11 08:28:55 +0530

    Item class added again

--- app/src/main/java/com/example/hangout/Models/Item.java
+++ app/src/main/java/com/example/hangout/Models/Item.java
@@ -1,4 +1,12 @@
 package com.example.hangout.Models;
 
 public class Item {
+    public int IID;
+    public String ItemName;
+   public double ItemPrice;
+   public  String         ItemQuntity;
+  public String ItemImage;
+    public   int      ItemCartValue;
+   public int MenuId;
+
 }
